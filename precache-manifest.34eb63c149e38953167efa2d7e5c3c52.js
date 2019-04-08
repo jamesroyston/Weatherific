065cba38a982bc6eec385f6557677f0db847f663
jamesroyston <jamesroyston03@gmail.com>
2019-04-08 10:49:36 -0700

    Updates

---
+++ precache-manifest.34eb63c149e38953167efa2d7e5c3c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b34a834ceb67b44df594b3668572e16",
+    "url": "/Weatherific/static/media/Stellar-light.7b34a834.otf"
+  },
+  {
+    "revision": "1e32d24acfe5ba1f591b",
+    "url": "/Weatherific/static/js/runtime~main.f3c706f5.js"
+  },
+  {
+    "revision": "b3d0b4500b18374dd483",
+    "url": "/Weatherific/static/js/main.b49ca8bc.chunk.js"
+  },
+  {
+    "revision": "38eeff9741a8794d4147",
+    "url": "/Weatherific/static/js/2.fed86b58.chunk.js"
+  },
+  {
+    "revision": "b3d0b4500b18374dd483",
+    "url": "/Weatherific/static/css/main.0554fb97.chunk.css"
+  },
+  {
+    "revision": "45a76bc368cea3939c4d343d1fce1703",
+    "url": "/Weatherific/index.html"
+  }
+];
