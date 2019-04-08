1a522e6f80aa23ec7a2506dc45ac4425d0f55317
jamesroyston <jamesroyston03@gmail.com>
2019-04-08 11:28:39 -0700

    file structure cleanup..

---
+++ myapp/build/precache-manifest.126183f487d19e97996cd22558e0a2ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7b34a834ceb67b44df594b3668572e16",
+    "url": "/Weatherific/static/media/Stellar-light.7b34a834.otf"
+  },
+  {
+    "revision": "1e32d24acfe5ba1f591b",
+    "url": "/Weatherific/static/js/runtime~main.f3c706f5.js"
+  },
+  {
+    "revision": "478628019fa7f18de68b",
+    "url": "/Weatherific/static/js/main.af0734a8.chunk.js"
+  },
+  {
+    "revision": "38eeff9741a8794d4147",
+    "url": "/Weatherific/static/js/2.fed86b58.chunk.js"
+  },
+  {
+    "revision": "478628019fa7f18de68b",
+    "url": "/Weatherific/static/css/main.798528f2.chunk.css"
+  },
+  {
+    "revision": "fc2357a2c696a3bc5f68103bf32f569b",
+    "url": "/Weatherific/index.html"
+  }
+];
